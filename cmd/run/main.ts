6520d978e1602ccb550f1fd5b8482b8f7739ac39
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-12-05 18:47:12 -0800

    inherit stdin and stdout in run task

--- cmd/run/main.ts
+++ cmd/run/main.ts
@@ -4,16 +4,13 @@ if (import.meta.main) {
   const est = parseESTFromArgs();
 
   // Create a child process using Deno.Command, running the script.
-  const child = new Deno.Command(Deno.execPath(), {
+  new Deno.Command(Deno.execPath(), {
     args: [
       "run",
       "-A",
       `./${est.year}/${est.day.toString().padStart(2, "0")}/main.ts`,
     ],
-    stdin: "piped",
-    stdout: "piped",
+    stdin: "inherit",
+    stdout: "inherit",
   }).spawn();
-
-  // Pipe the current process stdin to the child process stdin.
-  child.stdout.pipeTo(Deno.stdout.writable);
 }
