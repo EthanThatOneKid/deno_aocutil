104692d780fdd74064fe416be8c7df70b3f9a5bb
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-12-20 00:05:55 -0800

    2023 day 19 no attempt

---
+++ 2023/19/main.ts
@@ -0,0 +1,30 @@
+import * as aocutil from "aocutil/aocutil.ts";
+import * as aocapi from "aocutil/aocapi.ts";
+
+// Define constants.
+
+if (import.meta.main) {
+  performance.mark("part1");
+  const part1Solution = await part1();
+  
+  performance.mark("part2");
+  const part2Solution = await part2();
+  
+  performance.mark("end");
+  const part1Measure = performance.measure("part1", "part1", "part2");
+  const part2Measure = performance.measure("part2", "part2", "end");
+  console.table({
+    "Part 1": { solution: part1Solution, "time (ms)": part1Measure.duration },
+    "Part 2": { solution: part2Solution, "time (ms)": part2Measure.duration },
+  });
+
+  // await aocapi.submitPart1({ year: "2023", day: "19" }, part1Solution);
+  // await aocapi.submitPart2({ year: "2023", day: "19" }, part2Solution);
+}
+
+function part1() {
+  const input = aocutil.readFile("./2023/19/input");
+}
+
+function part2() {
+}
